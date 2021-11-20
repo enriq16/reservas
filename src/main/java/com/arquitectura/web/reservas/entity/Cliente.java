81a727cd9920d7d0c286322feb0511c0562066c2
enriq_16 <enriq16@fpuna.edu.py>
2021-11-20 13:07:54 -0300

    Agrego Entity para las tablas y archivos configuracion de persistencia

---
+++ src/main/java/com/arquitectura/web/reservas/entity/Cliente.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arquitectura.web.reservas.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Entity
+@Table(catalog = "bd_reservas", schema = "public", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"cedula"})})
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c"),
+    @NamedQuery(name = "Cliente.findById", query = "SELECT c FROM Cliente c WHERE c.id = :id"),
+    @NamedQuery(name = "Cliente.findByCedula", query = "SELECT c FROM Cliente c WHERE c.cedula = :cedula"),
+    @NamedQuery(name = "Cliente.findByNombre", query = "SELECT c FROM Cliente c WHERE c.nombre = :nombre"),
+    @NamedQuery(name = "Cliente.findByApellido", query = "SELECT c FROM Cliente c WHERE c.apellido = :apellido")})
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(nullable = false)
+    private int cedula;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(nullable = false, length = 100)
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(nullable = false, length = 100)
+    private String apellido;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idCliente", fetch = FetchType.EAGER)
+    private List<ReservaMesas> reservaMesasList;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente(Integer id, int cedula, String nombre, String apellido) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @XmlTransient
+    public List<ReservaMesas> getReservaMesasList() {
+        return reservaMesasList;
+    }
+
+    public void setReservaMesasList(List<ReservaMesas> reservaMesasList) {
+        this.reservaMesasList = reservaMesasList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.arquitectura.web.reservas.entity.Cliente[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/com/arquitectura/web/reservas/entity/ReservaMesas.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arquitectura.web.reservas.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Entity
+@Table(name = "reserva_mesas", catalog = "bd_reservas", schema = "public")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ReservaMesas.findAll", query = "SELECT r FROM ReservaMesas r"),
+    @NamedQuery(name = "ReservaMesas.findByIdReserva", query = "SELECT r FROM ReservaMesas r WHERE r.idReserva = :idReserva"),
+    @NamedQuery(name = "ReservaMesas.findByFecha", query = "SELECT r FROM ReservaMesas r WHERE r.fecha = :fecha"),
+    @NamedQuery(name = "ReservaMesas.findByRangoHora", query = "SELECT r FROM ReservaMesas r WHERE r.rangoHora = :rangoHora"),
+    @NamedQuery(name = "ReservaMesas.findByCantidadSolicitada", query = "SELECT r FROM ReservaMesas r WHERE r.cantidadSolicitada = :cantidadSolicitada")})
+public class ReservaMesas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_reserva", nullable = false)
+    private Integer idReserva;
+    @Basic(optional = false)
+    @NotNull
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "rango_hora", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date rangoHora;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "cantidad_solicitada", nullable = false)
+    private int cantidadSolicitada;
+    @JoinColumn(name = "id_cliente", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Cliente idCliente;
+    @JoinColumn(name = "id_mesa", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Mesas idMesa;
+    @JoinColumn(name = "id_restaurante", referencedColumnName = "id_restaurante", nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Restaurante idRestaurante;
+
+    public ReservaMesas() {
+    }
+
+    public ReservaMesas(Integer idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public ReservaMesas(Integer idReserva, Date fecha, Date rangoHora, int cantidadSolicitada) {
+        this.idReserva = idReserva;
+        this.fecha = fecha;
+        this.rangoHora = rangoHora;
+        this.cantidadSolicitada = cantidadSolicitada;
+    }
+
+    public Integer getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(Integer idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getRangoHora() {
+        return rangoHora;
+    }
+
+    public void setRangoHora(Date rangoHora) {
+        this.rangoHora = rangoHora;
+    }
+
+    public int getCantidadSolicitada() {
+        return cantidadSolicitada;
+    }
+
+    public void setCantidadSolicitada(int cantidadSolicitada) {
+        this.cantidadSolicitada = cantidadSolicitada;
+    }
+
+    public Cliente getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Cliente idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Mesas getIdMesa() {
+        return idMesa;
+    }
+
+    public void setIdMesa(Mesas idMesa) {
+        this.idMesa = idMesa;
+    }
+
+    public Restaurante getIdRestaurante() {
+        return idRestaurante;
+    }
+
+    public void setIdRestaurante(Restaurante idRestaurante) {
+        this.idRestaurante = idRestaurante;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idReserva != null ? idReserva.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ReservaMesas)) {
+            return false;
+        }
+        ReservaMesas other = (ReservaMesas) object;
+        if ((this.idReserva == null && other.idReserva != null) || (this.idReserva != null && !this.idReserva.equals(other.idReserva))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.arquitectura.web.reservas.entity.ReservaMesas[ idReserva=" + idReserva + " ]";
+    }
+    
+}
